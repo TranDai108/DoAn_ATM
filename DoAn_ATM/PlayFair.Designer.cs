14fe780447789f9937a4adb27eeecaa42a8e0206
Anh Lê Ngọc Kiều <22520047@ms.uit.edu.vn>
2024-12-13 11:30:30 +0700

    commit

--- DoAn_ATM/PlayFair.Designer.cs
+++ DoAn_ATM/PlayFair.Designer.cs
@@ -28,12 +28,549 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PlayFair";
+            label = new Label();
+            labelText = new Label();
+            labelKey = new Label();
+            tbText = new TextBox();
+            labelKeymatrix = new Label();
+            btEncrypt = new Button();
+            btDecrypt = new Button();
+            btClear = new Button();
+            tbKey = new TextBox();
+            tb00 = new TextBox();
+            tb01 = new TextBox();
+            tb02 = new TextBox();
+            tb03 = new TextBox();
+            tb04 = new TextBox();
+            tb05 = new TextBox();
+            tb15 = new TextBox();
+            tb14 = new TextBox();
+            tb13 = new TextBox();
+            tb12 = new TextBox();
+            tb11 = new TextBox();
+            tb10 = new TextBox();
+            tb25 = new TextBox();
+            tb24 = new TextBox();
+            tb23 = new TextBox();
+            tb22 = new TextBox();
+            tb21 = new TextBox();
+            tb20 = new TextBox();
+            tb35 = new TextBox();
+            tb34 = new TextBox();
+            tb33 = new TextBox();
+            tb32 = new TextBox();
+            tb31 = new TextBox();
+            tb30 = new TextBox();
+            tb45 = new TextBox();
+            tb44 = new TextBox();
+            tb43 = new TextBox();
+            tb42 = new TextBox();
+            tb41 = new TextBox();
+            tb40 = new TextBox();
+            bt5x5 = new Button();
+            bt6x6 = new Button();
+            labelResult = new Label();
+            tbResult = new TextBox();
+            pnlMatrix = new Panel();
+            tb55 = new TextBox();
+            tb54 = new TextBox();
+            tb53 = new TextBox();
+            tb52 = new TextBox();
+            tb51 = new TextBox();
+            tb50 = new TextBox();
+            pnlMatrix.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Font = new Font("Segoe Script", 13.875F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label.Location = new Point(601, 31);
+            label.Name = "label";
+            label.Size = new Size(232, 61);
+            label.TabIndex = 0;
+            label.Text = "PLAYFAIR";
+            // 
+            // labelText
+            // 
+            labelText.AutoSize = true;
+            labelText.Location = new Point(122, 167);
+            labelText.Name = "labelText";
+            labelText.Size = new Size(57, 32);
+            labelText.TabIndex = 1;
+            labelText.Text = "Text";
+            // 
+            // labelKey
+            // 
+            labelKey.AutoSize = true;
+            labelKey.Location = new Point(94, 283);
+            labelKey.Name = "labelKey";
+            labelKey.Size = new Size(140, 32);
+            labelKey.TabIndex = 2;
+            labelKey.Text = "PlayFair Key";
+            // 
+            // tbText
+            // 
+            tbText.Location = new Point(197, 164);
+            tbText.Name = "tbText";
+            tbText.Size = new Size(336, 39);
+            tbText.TabIndex = 3;
+            // 
+            // labelKeymatrix
+            // 
+            labelKeymatrix.AutoSize = true;
+            labelKeymatrix.Location = new Point(94, 400);
+            labelKeymatrix.Name = "labelKeymatrix";
+            labelKeymatrix.Size = new Size(127, 32);
+            labelKeymatrix.TabIndex = 4;
+            labelKeymatrix.Text = "Key Matrix";
+            // 
+            // btEncrypt
+            // 
+            btEncrypt.Location = new Point(956, 339);
+            btEncrypt.Name = "btEncrypt";
+            btEncrypt.Size = new Size(131, 54);
+            btEncrypt.TabIndex = 5;
+            btEncrypt.Text = "Encrypt";
+            btEncrypt.UseVisualStyleBackColor = true;
+            btEncrypt.Click += btEncrypt_Click;
+            // 
+            // btDecrypt
+            // 
+            btDecrypt.Location = new Point(934, 476);
+            btDecrypt.Name = "btDecrypt";
+            btDecrypt.Size = new Size(133, 60);
+            btDecrypt.TabIndex = 6;
+            btDecrypt.Text = "Decrypt";
+            btDecrypt.UseVisualStyleBackColor = true;
+            btDecrypt.Click += btDecrypt_Click;
+            // 
+            // btClear
+            // 
+            btClear.Location = new Point(968, 666);
+            btClear.Name = "btClear";
+            btClear.Size = new Size(142, 55);
+            btClear.TabIndex = 7;
+            btClear.Text = "Clear";
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Click += btClear_Click;
+            // 
+            // tbKey
+            // 
+            tbKey.Location = new Point(261, 284);
+            tbKey.Name = "tbKey";
+            tbKey.Size = new Size(293, 39);
+            tbKey.TabIndex = 10;
+            // 
+            // tb00
+            // 
+            tb00.Location = new Point(20, 20);
+            tb00.Name = "tb00";
+            tb00.Size = new Size(49, 39);
+            tb00.TabIndex = 12;
+            // 
+            // tb01
+            // 
+            tb01.Location = new Point(87, 20);
+            tb01.Name = "tb01";
+            tb01.Size = new Size(49, 39);
+            tb01.TabIndex = 13;
+            // 
+            // tb02
+            // 
+            tb02.Location = new Point(151, 20);
+            tb02.Name = "tb02";
+            tb02.Size = new Size(49, 39);
+            tb02.TabIndex = 14;
+            // 
+            // tb03
+            // 
+            tb03.Location = new Point(217, 20);
+            tb03.Name = "tb03";
+            tb03.Size = new Size(49, 39);
+            tb03.TabIndex = 15;
+            // 
+            // tb04
+            // 
+            tb04.Location = new Point(285, 20);
+            tb04.Name = "tb04";
+            tb04.Size = new Size(49, 39);
+            tb04.TabIndex = 16;
+            // 
+            // tb05
+            // 
+            tb05.Location = new Point(352, 20);
+            tb05.Name = "tb05";
+            tb05.Size = new Size(49, 39);
+            tb05.TabIndex = 17;
+            // 
+            // tb15
+            // 
+            tb15.Location = new Point(352, 78);
+            tb15.Name = "tb15";
+            tb15.Size = new Size(49, 39);
+            tb15.TabIndex = 23;
+            // 
+            // tb14
+            // 
+            tb14.Location = new Point(285, 78);
+            tb14.Name = "tb14";
+            tb14.Size = new Size(49, 39);
+            tb14.TabIndex = 22;
+            // 
+            // tb13
+            // 
+            tb13.Location = new Point(217, 78);
+            tb13.Name = "tb13";
+            tb13.Size = new Size(49, 39);
+            tb13.TabIndex = 21;
+            // 
+            // tb12
+            // 
+            tb12.Location = new Point(151, 78);
+            tb12.Name = "tb12";
+            tb12.Size = new Size(49, 39);
+            tb12.TabIndex = 20;
+            // 
+            // tb11
+            // 
+            tb11.Location = new Point(87, 78);
+            tb11.Name = "tb11";
+            tb11.Size = new Size(49, 39);
+            tb11.TabIndex = 19;
+            // 
+            // tb10
+            // 
+            tb10.Location = new Point(20, 78);
+            tb10.Name = "tb10";
+            tb10.Size = new Size(49, 39);
+            tb10.TabIndex = 18;
+            // 
+            // tb25
+            // 
+            tb25.Location = new Point(352, 139);
+            tb25.Name = "tb25";
+            tb25.Size = new Size(49, 39);
+            tb25.TabIndex = 29;
+            // 
+            // tb24
+            // 
+            tb24.Location = new Point(285, 139);
+            tb24.Name = "tb24";
+            tb24.Size = new Size(49, 39);
+            tb24.TabIndex = 28;
+            // 
+            // tb23
+            // 
+            tb23.Location = new Point(217, 139);
+            tb23.Name = "tb23";
+            tb23.Size = new Size(49, 39);
+            tb23.TabIndex = 27;
+            // 
+            // tb22
+            // 
+            tb22.Location = new Point(151, 139);
+            tb22.Name = "tb22";
+            tb22.Size = new Size(49, 39);
+            tb22.TabIndex = 26;
+            // 
+            // tb21
+            // 
+            tb21.Location = new Point(87, 139);
+            tb21.Name = "tb21";
+            tb21.Size = new Size(49, 39);
+            tb21.TabIndex = 25;
+            // 
+            // tb20
+            // 
+            tb20.Location = new Point(20, 139);
+            tb20.Name = "tb20";
+            tb20.Size = new Size(49, 39);
+            tb20.TabIndex = 24;
+            // 
+            // tb35
+            // 
+            tb35.Location = new Point(352, 201);
+            tb35.Name = "tb35";
+            tb35.Size = new Size(49, 39);
+            tb35.TabIndex = 35;
+            // 
+            // tb34
+            // 
+            tb34.Location = new Point(285, 201);
+            tb34.Name = "tb34";
+            tb34.Size = new Size(49, 39);
+            tb34.TabIndex = 34;
+            // 
+            // tb33
+            // 
+            tb33.Location = new Point(217, 201);
+            tb33.Name = "tb33";
+            tb33.Size = new Size(49, 39);
+            tb33.TabIndex = 33;
+            // 
+            // tb32
+            // 
+            tb32.Location = new Point(151, 201);
+            tb32.Name = "tb32";
+            tb32.Size = new Size(49, 39);
+            tb32.TabIndex = 32;
+            // 
+            // tb31
+            // 
+            tb31.Location = new Point(87, 201);
+            tb31.Name = "tb31";
+            tb31.Size = new Size(49, 39);
+            tb31.TabIndex = 31;
+            // 
+            // tb30
+            // 
+            tb30.Location = new Point(20, 201);
+            tb30.Name = "tb30";
+            tb30.Size = new Size(49, 39);
+            tb30.TabIndex = 30;
+            // 
+            // tb45
+            // 
+            tb45.Location = new Point(352, 264);
+            tb45.Name = "tb45";
+            tb45.Size = new Size(49, 39);
+            tb45.TabIndex = 41;
+            // 
+            // tb44
+            // 
+            tb44.Location = new Point(285, 264);
+            tb44.Name = "tb44";
+            tb44.Size = new Size(49, 39);
+            tb44.TabIndex = 40;
+            // 
+            // tb43
+            // 
+            tb43.Location = new Point(217, 264);
+            tb43.Name = "tb43";
+            tb43.Size = new Size(49, 39);
+            tb43.TabIndex = 39;
+            // 
+            // tb42
+            // 
+            tb42.Location = new Point(151, 264);
+            tb42.Name = "tb42";
+            tb42.Size = new Size(49, 39);
+            tb42.TabIndex = 38;
+            // 
+            // tb41
+            // 
+            tb41.Location = new Point(87, 264);
+            tb41.Name = "tb41";
+            tb41.Size = new Size(49, 39);
+            tb41.TabIndex = 37;
+            // 
+            // tb40
+            // 
+            tb40.Location = new Point(20, 264);
+            tb40.Name = "tb40";
+            tb40.Size = new Size(49, 39);
+            tb40.TabIndex = 36;
+            // 
+            // bt5x5
+            // 
+            bt5x5.Location = new Point(573, 473);
+            bt5x5.Name = "bt5x5";
+            bt5x5.Size = new Size(162, 63);
+            bt5x5.TabIndex = 42;
+            bt5x5.Text = "5x5 matrix";
+            bt5x5.UseVisualStyleBackColor = true;
+            bt5x5.Click += bt5x5_Click;
+            // 
+            // bt6x6
+            // 
+            bt6x6.Location = new Point(573, 569);
+            bt6x6.Name = "bt6x6";
+            bt6x6.Size = new Size(162, 63);
+            bt6x6.TabIndex = 43;
+            bt6x6.Text = "6x6 matrix";
+            bt6x6.UseVisualStyleBackColor = true;
+            bt6x6.Click += bt6x6_Click;
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(956, 164);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(78, 32);
+            labelResult.TabIndex = 44;
+            labelResult.Text = "Result";
+            // 
+            // tbResult
+            // 
+            tbResult.Location = new Point(1051, 160);
+            tbResult.Name = "tbResult";
+            tbResult.Size = new Size(317, 39);
+            tbResult.TabIndex = 45;
+            // 
+            // pnlMatrix
+            // 
+            pnlMatrix.Controls.Add(tb55);
+            pnlMatrix.Controls.Add(tb54);
+            pnlMatrix.Controls.Add(tb53);
+            pnlMatrix.Controls.Add(tb52);
+            pnlMatrix.Controls.Add(tb51);
+            pnlMatrix.Controls.Add(tb50);
+            pnlMatrix.Controls.Add(tb45);
+            pnlMatrix.Controls.Add(tb44);
+            pnlMatrix.Controls.Add(tb43);
+            pnlMatrix.Controls.Add(tb42);
+            pnlMatrix.Controls.Add(tb41);
+            pnlMatrix.Controls.Add(tb40);
+            pnlMatrix.Controls.Add(tb35);
+            pnlMatrix.Controls.Add(tb34);
+            pnlMatrix.Controls.Add(tb33);
+            pnlMatrix.Controls.Add(tb32);
+            pnlMatrix.Controls.Add(tb31);
+            pnlMatrix.Controls.Add(tb30);
+            pnlMatrix.Controls.Add(tb25);
+            pnlMatrix.Controls.Add(tb24);
+            pnlMatrix.Controls.Add(tb23);
+            pnlMatrix.Controls.Add(tb22);
+            pnlMatrix.Controls.Add(tb21);
+            pnlMatrix.Controls.Add(tb20);
+            pnlMatrix.Controls.Add(tb15);
+            pnlMatrix.Controls.Add(tb14);
+            pnlMatrix.Controls.Add(tb13);
+            pnlMatrix.Controls.Add(tb12);
+            pnlMatrix.Controls.Add(tb11);
+            pnlMatrix.Controls.Add(tb10);
+            pnlMatrix.Controls.Add(tb05);
+            pnlMatrix.Controls.Add(tb04);
+            pnlMatrix.Controls.Add(tb03);
+            pnlMatrix.Controls.Add(tb02);
+            pnlMatrix.Controls.Add(tb01);
+            pnlMatrix.Controls.Add(tb00);
+            pnlMatrix.Location = new Point(85, 454);
+            pnlMatrix.Name = "pnlMatrix";
+            pnlMatrix.Size = new Size(433, 392);
+            pnlMatrix.TabIndex = 46;
+            // 
+            // tb55
+            // 
+            tb55.Location = new Point(352, 325);
+            tb55.Name = "tb55";
+            tb55.Size = new Size(49, 39);
+            tb55.TabIndex = 47;
+            // 
+            // tb54
+            // 
+            tb54.Location = new Point(285, 325);
+            tb54.Name = "tb54";
+            tb54.Size = new Size(49, 39);
+            tb54.TabIndex = 46;
+            // 
+            // tb53
+            // 
+            tb53.Location = new Point(217, 325);
+            tb53.Name = "tb53";
+            tb53.Size = new Size(49, 39);
+            tb53.TabIndex = 45;
+            // 
+            // tb52
+            // 
+            tb52.Location = new Point(151, 325);
+            tb52.Name = "tb52";
+            tb52.Size = new Size(49, 39);
+            tb52.TabIndex = 44;
+            // 
+            // tb51
+            // 
+            tb51.Location = new Point(87, 325);
+            tb51.Name = "tb51";
+            tb51.Size = new Size(49, 39);
+            tb51.TabIndex = 43;
+            // 
+            // tb50
+            // 
+            tb50.Location = new Point(20, 325);
+            tb50.Name = "tb50";
+            tb50.Size = new Size(49, 39);
+            tb50.TabIndex = 42;
+            // 
+            // PlayFair
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1438, 885);
+            Controls.Add(pnlMatrix);
+            Controls.Add(tbResult);
+            Controls.Add(labelResult);
+            Controls.Add(bt6x6);
+            Controls.Add(bt5x5);
+            Controls.Add(tbKey);
+            Controls.Add(btClear);
+            Controls.Add(btDecrypt);
+            Controls.Add(btEncrypt);
+            Controls.Add(labelKeymatrix);
+            Controls.Add(tbText);
+            Controls.Add(labelKey);
+            Controls.Add(labelText);
+            Controls.Add(label);
+            Name = "PlayFair";
+            Text = "PlayFair";
+            pnlMatrix.ResumeLayout(false);
+            pnlMatrix.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label;
+        private Label labelText;
+        private Label labelKey;
+        private TextBox tbText;
+        private Label labelKeymatrix;
+        private Button btEncrypt;
+        private Button btDecrypt;
+        private Button btClear;
+        private TextBox tbKey;
+        private TextBox tb00;
+        private TextBox tb01;
+        private TextBox tb02;
+        private TextBox tb03;
+        private TextBox tb04;
+        private TextBox tb05;
+        private TextBox tb15;
+        private TextBox tb14;
+        private TextBox tb13;
+        private TextBox tb12;
+        private TextBox tb11;
+        private TextBox tb10;
+        private TextBox tb25;
+        private TextBox tb24;
+        private TextBox tb23;
+        private TextBox tb22;
+        private TextBox tb21;
+        private TextBox tb20;
+        private TextBox tb35;
+        private TextBox tb34;
+        private TextBox tb33;
+        private TextBox tb32;
+        private TextBox tb31;
+        private TextBox tb30;
+        private TextBox tb45;
+        private TextBox tb44;
+        private TextBox tb43;
+        private TextBox tb42;
+        private TextBox tb41;
+        private TextBox tb40;
+        private Button bt5x5;
+        private Button bt6x6;
+        private Label labelResult;
+        private TextBox tbResult;
+        private Panel pnlMatrix;
+        private TextBox tb55;
+        private TextBox tb54;
+        private TextBox tb53;
+        private TextBox tb52;
+        private TextBox tb51;
+        private TextBox tb50;
     }
 }
--- DoAn_ATM/PlayFair.cs
+++ DoAn_ATM/PlayFair.cs
@@ -7,14 +7,217 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace DoAn_ATM
 {
     public partial class PlayFair : Form
     {
+        private char[,] keyMatrix;
+        private bool is5x5Matrix = true; // Default matrix size is 5x5
         public PlayFair()
         {
             InitializeComponent();
+        }
+
+        private char[,] CreateMatrix(string key)
+        {
+            int size = is5x5Matrix ? 5 : 6;
+            char[,] matrix = new char[size, size];
+            key = new string(key.ToUpper().Where(char.IsLetter).Distinct().ToArray());
+
+            if (is5x5Matrix)
+            {
+                key = key.Replace("J", "I");
+            }
+
+            string alphabet = is5x5Matrix ? "ABCDEFGHIKLMNOPQRSTUVWXYZ" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string fullKey = key + new string(alphabet.Where(c => !key.Contains(c)).ToArray());
+
+            for (int i = 0, k = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = fullKey[k++];
+                }
+            }
+
+            UpdateMatrixUI(matrix); 
+            return matrix;
+        }
+
+        private string FormatText(string text)
+        {
+            text = new string(text.ToUpper().Where(char.IsLetterOrDigit).ToArray());
+
+            if (is5x5Matrix)
+            {
+                text = text.Replace("J", "I");
+            }
+
+            for (int i = 0; i < text.Length - 1; i += 2)
+            {
+                if (text[i] == text[i + 1])
+                {
+                    text = text.Insert(i + 1, is5x5Matrix ? "X" : "Z");
+                }
+            }
+
+            if (text.Length % 2 != 0)
+            {
+                text += is5x5Matrix ? "X" : "Z";
+            }
+            return text;
+        }
+
+        private string Encrypt(string text)
+        {
+            return ProcessText(text, true);
+        }
+
+        private string Decrypt(string text)
+        {
+            return ProcessText(text, false);
+        }
+
+        private string ProcessText(string text, bool encrypt)
+        {
+            string result = "";
+            int size = is5x5Matrix ? 5 : 6;
+
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                char a = text[i];
+                char b = text[i + 1];
+
+                int rowA = 0, colA = 0, rowB = 0, colB = 0;
+                FindPosition(a, ref rowA, ref colA);
+                FindPosition(b, ref rowB, ref colB);
+
+                if (rowA == rowB)
+                {
+                    colA = (colA + (encrypt ? 1 : -1) + size) % size;
+                    colB = (colB + (encrypt ? 1 : -1) + size) % size;
+                }
+                else if (colA == colB)
+                {
+                    rowA = (rowA + (encrypt ? 1 : -1) + size) % size;
+                    rowB = (rowB + (encrypt ? 1 : -1) + size) % size;
+                }
+                else
+                {
+                    int temp = colA;
+                    colA = colB;
+                    colB = temp;
+                }
+                result += keyMatrix[rowA, colA].ToString() + keyMatrix[rowB, colB].ToString();
+            }
+            return result;
+        }
+
+
+        private void FindPosition(char c, ref int row, ref int col)
+        {
+            int size = is5x5Matrix ? 5 : 6;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (keyMatrix[i, j] == c)
+                    {
+                        row = i;
+                        col = j;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void UpdateMatrixUI(char[,] matrix)
+        {
+            int size = matrix.GetLength(0); // 5x5 hoặc 6x6 tùy chọn
+
+            foreach (Control control in pnlMatrix.Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Text = string.Empty; // Xóa nội dung cũ
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    // Lấy TextBox theo tên định dạng txtXY
+                    string textBoxName = $"tb{i}{j}";
+                    TextBox box = pnlMatrix.Controls[textBoxName] as TextBox;
+
+                    if (box != null)
+                    {
+                        box.Text = matrix[i, j].ToString(); // Hiển thị ký tự từ ma trận
+                    }
+                }
+            }
+        }
+
+        private void btEncrypt_Click(object sender, EventArgs e)
+        {
+            string key = tbKey.Text;
+            string text = tbText.Text;
+
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Please enter both text and key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            keyMatrix = CreateMatrix(key);
+            string formattedText = FormatText(text);
+            tbResult.Text = Encrypt(formattedText);
+        }
+
+        private void btDecrypt_Click(object sender, EventArgs e)
+        {
+            string key = tbKey.Text;
+            string text = tbText.Text;
+
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("Please enter both text and key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            keyMatrix = CreateMatrix(key);
+            tbResult.Text = Decrypt(text);
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            tbText.Clear();
+            tbKey.Clear();
+            tbResult.Clear();
+            ClearMatrix();
+        }
+        private void bt5x5_Click(object sender, EventArgs e)
+        {
+            is5x5Matrix = true;
+            MessageBox.Show("Matrix size set to 5x5.", "Matrix Size", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void bt6x6_Click(object sender, EventArgs e)
+        {
+            is5x5Matrix = false;
+            MessageBox.Show("Matrix size set to 6x6.", "Matrix Size", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void ClearMatrix()
+        {
+            foreach (Control control in pnlMatrix.Controls)
+            {
+                if (control is TextBox textBox)
+                {
+                    textBox.Text = string.Empty;
+                }
+            }
         }
     }
 }
